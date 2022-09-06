0f7d4ae768edf87cbba453982e3499b420e4bbb3
James Harvey <44349936+jmshrv@users.noreply.github.com>
2022-09-06 13:17:38 +0100

    Start work on zigzag conversion

---
+++ 6-zigzag-conversion/zigzag-conversion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int num_cols(int s_len, int numRows)
+{
+}
+
+char *convert(char *s, int numRows)
+{
+    char **rows[numRows][]
+}
+
+int main()
+{
+    printf("%s\n", convert("PAYPALISHIRING", 3));
+}
