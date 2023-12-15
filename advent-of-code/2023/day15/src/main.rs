6143f7bd529f6e76fc70ec056e4773efeacda068
James Harvey <44349936+jmshrv@users.noreply.github.com>
2023-12-15 21:33:26 +0000

    AoC 2023 Day 15 Part 2

--- advent-of-code/2023/day15/src/main.rs
+++ advent-of-code/2023/day15/src/main.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::{BTreeMap, HashMap},
-    io,
-};
+use std::{collections::HashMap, io};
 
 fn hash(input: &str) -> u64 {
     let mut res = 0;
@@ -19,10 +16,12 @@ fn part2(input: &Vec<&str>) -> u64 {
     let mut map: HashMap<u64, Vec<(&str, u64)>> = HashMap::new();
 
     for step in input {
-        let (identifier, value) = step.split_once(['-', '=']).expect("Failed to split step!");
+        let (identifier, value_str) = step.split_once(['-', '=']).expect("Failed to split step!");
+        let value = value_str.parse::<u64>().unwrap_or(0);
+        let hash_key = hash(identifier);
 
         if step.ends_with('-') {
-            if let Some(lens_box) = map.get_mut(&hash(*step)) {
+            if let Some(lens_box) = map.get_mut(&hash_key) {
                 if let Some(index) = lens_box
                     .iter()
                     .position(|(box_id, _)| *box_id == identifier)
@@ -30,10 +29,30 @@ fn part2(input: &Vec<&str>) -> u64 {
                     lens_box.remove(index);
                 }
             }
+        } else {
+            map.entry(hash_key)
+                .and_modify(|lens_box| {
+                    if let Some(lens) = lens_box.iter_mut().find(|index| index.0 == identifier) {
+                        lens.1 = value;
+                    } else {
+                        lens_box.push((identifier, value));
+                    }
+                })
+                .or_insert(vec![(identifier, value)]);
         }
     }
 
-    todo!()
+    map.iter()
+        .map(|(box_number, lens_box)| {
+            lens_box
+                .iter()
+                .enumerate()
+                .map(|(index, (_, focal_length))| {
+                    (box_number + 1) * (index as u64 + 1) * focal_length
+                })
+                .sum::<u64>()
+        })
+        .sum()
 }
 
 fn main() {
@@ -46,8 +65,8 @@ fn main() {
     let split_input = input.split(',').collect::<Vec<_>>();
 
     let part1_answer: u64 = split_input.iter().map(|step| hash(step)).sum();
-
     println!("{part1_answer}");
 
-    let mut part2_answer = part2(&split_input);
+    let part2_answer = part2(&split_input);
+    println!("{part2_answer}");
 }
