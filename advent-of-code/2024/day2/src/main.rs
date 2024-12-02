4cbb492f6b0ac74aa3ca209fccc2ba1dc71f95ad
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-12-02 10:06:14 +0000

    AoC 2024 Day 2 Part 2

--- advent-of-code/2024/day2/src/main.rs
+++ advent-of-code/2024/day2/src/main.rs
@@ -2,6 +2,23 @@ use std::{cmp::Ordering, io};
 
 use itertools::Itertools;
 
+fn is_reactor_valid(reactor: &[i32]) -> bool {
+    let is_ordering_valid = reactor
+        .iter()
+        .tuple_windows::<(_, _)>()
+        .map(|(a, b)| a.cmp(b))
+        .filter(|ord| *ord != Ordering::Equal)
+        .all_equal();
+
+    let is_difference_valid = reactor
+        .iter()
+        .tuple_windows::<(_, _)>()
+        .map(|(a, b)| (a - b).abs())
+        .all(|difference| difference >= 1 && difference <= 3);
+
+    is_ordering_valid && is_difference_valid
+}
+
 fn main() {
     let input = io::stdin()
         .lines()
@@ -17,20 +34,28 @@ fn main() {
 
     let part_one = input
         .iter()
-        .filter(|line| {
-            line.iter()
-                .tuple_windows::<(_, _)>()
-                .map(|(a, b)| a.cmp(b))
-                .filter(|ord| *ord != Ordering::Equal)
-                .all_equal()
-        })
-        .filter(|line| {
-            line.iter()
-                .tuple_windows::<(_, _)>()
-                .map(|(a, b)| (a - b).abs())
-                .all(|difference| difference >= 1 && difference <= 3)
-        })
+        .filter(|reactor| is_reactor_valid(reactor))
         .count();
 
     println!("{part_one}");
+
+    let part_two = input
+        .iter()
+        .filter(|reactor| {
+            if is_reactor_valid(reactor) {
+                return true;
+            }
+
+            for i in 0..reactor.len() {
+                let reactor_without_level = [&reactor[..i], &reactor[(i + 1)..]].concat();
+                if is_reactor_valid(&reactor_without_level) {
+                    return true;
+                }
+            }
+
+            return false;
+        })
+        .count();
+
+    println!("{part_two}");
 }
