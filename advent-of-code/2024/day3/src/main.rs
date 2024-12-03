0a476e51e2f3f6d88e299b50ab188f1fa002766b
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-12-03 08:48:01 +0000

    AoC 2024 Day 3 (terrible)

---
+++ advent-of-code/2024/day3/src/main.rs
@@ -0,0 +1,77 @@
+use std::{collections::HashSet, io};
+
+use regex::Regex;
+
+fn is_enabled(index: usize, dos: &HashSet<usize>, donts: &HashSet<usize>) -> bool {
+    // let last_do_index = dos
+    //     .iter()
+    //     .rev()
+    //     .find(|do_index| index >= **do_index)
+    //     .unwrap_or(&0);
+
+    // let last_dont_index = donts
+    //     .iter()
+    //     .rev()
+    //     .find(|dont_index| index >= **dont_index)
+    //     .unwrap_or(&0);
+
+    // println!("{index} {dos:?} {donts:?} {last_do_index} {last_dont_index}");
+
+    // let ans = last_do_index > last_dont_index;
+
+    // println!("{ans}");
+
+    // ans
+
+    let mut result = true;
+
+    for i in 0..index {
+        if dos.contains(&i) {
+            result = true;
+        } else if donts.contains(&i) {
+            result = false;
+        }
+    }
+
+    result
+}
+
+fn main() {
+    let input: String = io::stdin().lines().flatten().collect();
+
+    let regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+
+    let multiplications = regex
+        .captures_iter(&input)
+        .map(|captures| {
+            (
+                captures.get(0).unwrap().start(),
+                captures[1].parse::<u32>().unwrap(),
+                captures[2].parse::<u32>().unwrap(),
+            )
+        })
+        .map(|(index, a, b)| (index, a * b))
+        .collect::<Vec<_>>();
+
+    let part_one: u32 = multiplications.iter().map(|(_, result)| result).sum();
+
+    println!("{part_one}");
+
+    let dos = input
+        .match_indices("do()")
+        .map(|(index, _)| index)
+        .collect::<HashSet<_>>();
+
+    let donts = input
+        .match_indices("don't()")
+        .map(|(index, _)| index)
+        .collect::<HashSet<_>>();
+
+    let part_two: u32 = multiplications
+        .iter()
+        .filter(|(index, _)| is_enabled(*index, &dos, &donts))
+        .map(|(_, result)| result)
+        .sum();
+
+    println!("{part_two}");
+}
