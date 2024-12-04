045ab253dbe678797c7de1235c5c69e1813dbbb4
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-12-04 18:35:34 +0000

    AoC 2024 Day 4 Part 2

--- advent-of-code/2024/day4/src/main.rs
+++ advent-of-code/2024/day4/src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-fn search(wordsearch: &Vec<Vec<char>>, vector: (i32, i32), mut position: (i32, i32)) -> bool {
+fn part1_search(wordsearch: &Vec<Vec<char>>, vector: (i32, i32), mut position: (i32, i32)) -> bool {
     let goal = "XMAS";
     let mut actual = "X".to_string();
 
@@ -28,6 +28,33 @@ fn search(wordsearch: &Vec<Vec<char>>, vector: (i32, i32), mut position: (i32, i
     actual == goal
 }
 
+fn part2_search(wordsearch: &Vec<Vec<char>>, position: (usize, usize)) -> bool {
+    let vertical_len = wordsearch.len();
+    let horizontal_len = wordsearch.first().unwrap().len();
+
+    if position.0 == 0 || position.1 == 0 {
+        return false;
+    }
+
+    if position.0 >= vertical_len - 1 || position.1 >= horizontal_len - 1 {
+        return false;
+    }
+
+    let top_left = wordsearch[position.0 - 1][position.1 - 1];
+    let top_right = wordsearch[position.0 - 1][position.1 + 1];
+    let bottom_left = wordsearch[position.0 + 1][position.1 - 1];
+    let bottom_right = wordsearch[position.0 + 1][position.1 + 1];
+
+    let valid_perms = [
+        ['M', 'S', 'S', 'M'],
+        ['M', 'M', 'S', 'S'],
+        ['S', 'M', 'M', 'S'],
+        ['S', 'S', 'M', 'M'],
+    ];
+
+    valid_perms.contains(&[top_left, top_right, bottom_right, bottom_left])
+}
+
 fn main() {
     let input = io::stdin()
         .lines()
@@ -47,6 +74,7 @@ fn main() {
     ];
 
     let mut part_one = 0;
+    let mut part_two = 0;
 
     let xes = input
         .iter()
@@ -64,10 +92,32 @@ fn main() {
         for (x, _) in line {
             part_one += search_vectors
                 .iter()
-                .filter(|vector| search(&input, **vector, (y as i32, *x as i32)))
+                .filter(|vector| part1_search(&input, **vector, (y as i32, *x as i32)))
                 .count();
         }
     }
 
     println!("{part_one}");
+
+    let cross_middles = input
+        .iter()
+        .enumerate()
+        .map(|(_, line)| {
+            line.iter()
+                .enumerate()
+                .filter(|(_, char)| **char == 'A')
+                .collect::<Vec<_>>()
+        })
+        .filter(|line| !line.is_empty())
+        .collect::<Vec<_>>();
+
+    for (y, line) in cross_middles.iter().enumerate() {
+        for (x, _) in line {
+            if part2_search(&input, (y, *x)) {
+                part_two += 1;
+            }
+        }
+    }
+
+    println!("{part_two}");
 }
