5333dda768a1f92dc83bb6e21ba215edccfeb212
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-12-14 02:50:53 +0000

    Very broken sorta-Dijkstra

--- advent-of-code/2024/day13/src/main.rs
+++ advent-of-code/2024/day13/src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{cmp::Ordering, collections::BinaryHeap, io};
 
 use lazy_regex::regex;
 
@@ -7,6 +7,25 @@ struct Statement {
     button_a: (u64, u64),
     button_b: (u64, u64),
     prize: (u64, u64),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct State {
+    tokens: u64,
+    x: u64,
+    y: u64,
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.tokens.cmp(&self.tokens)
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 fn parse_statement(input: &str) -> Option<Statement> {
@@ -36,12 +55,42 @@ fn parse_statement(input: &str) -> Option<Statement> {
     })
 }
 
-fn button_presses(statement: &Statement) -> Option<(u64, u64)> {
-    let mut tokens = 0;
+fn button_presses(statement: &Statement) -> Option<u64> {
+    let mut queue = BinaryHeap::from([State {
+        tokens: 0,
+        x: 0,
+        y: 0,
+    }]);
 
-    // Dijkstra :D
+    while let Some(node) = queue.pop() {
+        if node.x == statement.prize.0 && node.y == statement.prize.1 {
+            return Some(node.tokens);
+        }
 
-    todo!()
+        let a_next = (node.x + statement.button_a.0, node.y + statement.button_a.1);
+        let b_next = (node.x + statement.button_b.0, node.y + statement.button_b.1);
+
+        // println!("{a_next:?}");
+        // println!("{b_next:?}");
+
+        if a_next.0 <= statement.prize.0 && a_next.1 <= statement.prize.1 {
+            queue.push(State {
+                tokens: node.tokens + 3,
+                x: a_next.0,
+                y: a_next.1,
+            });
+        }
+
+        if b_next.0 <= statement.prize.0 && b_next.1 <= statement.prize.1 {
+            queue.push(State {
+                tokens: node.tokens + 1,
+                x: b_next.0,
+                y: b_next.0,
+            });
+        }
+    }
+
+    None
 }
 
 fn main() {
@@ -50,4 +99,8 @@ fn main() {
         .split("\n\n")
         .map(|statement| parse_statement(statement).unwrap())
         .collect::<Vec<_>>();
+
+    for statement in input {
+        println!("{:?}", button_presses(&statement));
+    }
 }
