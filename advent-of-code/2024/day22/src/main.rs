d935b13199a1818c4ccf69216358d77739d881dc
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-12-23 03:59:14 +0000

    AoC 2024 Day 22 Part 2

--- advent-of-code/2024/day22/src/main.rs
+++ advent-of-code/2024/day22/src/main.rs
@@ -1,4 +1,9 @@
-use std::io;
+use itertools::Itertools;
+
+use std::{
+    collections::{HashMap, HashSet},
+    io,
+};
 
 fn mix(secret_number: u64, value: u64) -> u64 {
     value ^ secret_number
@@ -41,4 +46,50 @@ fn main() {
         .sum();
 
     println!("{part_one}");
+
+    let mut profits = HashMap::new();
+
+    let sellers = input
+        .iter()
+        .map(|initial_secret| {
+            let mut secret = *initial_secret;
+
+            let mut answer = vec![secret % 10];
+
+            for _ in 0..2000 {
+                secret = evolve(secret);
+                answer.push(secret % 10);
+            }
+
+            answer
+        })
+        .collect::<Vec<_>>();
+
+    for prices in &sellers {
+        let price_diffs = prices
+            .iter()
+            .tuple_windows()
+            .map(|(a, b)| (*b, *a as i64 - *b as i64));
+
+        let mut seen = HashSet::new();
+
+        for window in price_diffs.tuple_windows::<(_, _, _, _)>() {
+            let differences = (window.0 .1, window.1 .1, window.2 .1, window.3 .1);
+
+            if !seen.insert(differences) {
+                continue;
+            }
+
+            let price = window.3 .0;
+
+            profits
+                .entry(differences)
+                .and_modify(|diff_price| *diff_price += price)
+                .or_insert(price);
+        }
+    }
+
+    let part_two = profits.values().max().unwrap();
+
+    println!("{part_two}");
 }
