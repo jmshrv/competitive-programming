cb35db1f6dfcc179144999a4957cd1186f8bf809
James Harvey <44349936+jmshrv@users.noreply.github.com>
2025-01-01 21:27:01 +0000

    need to handle pressing A >:(

--- advent-of-code/2024/day21/src/main.rs
+++ advent-of-code/2024/day21/src/main.rs
@@ -1,36 +1,87 @@
 use std::{
-    cell::RefCell,
-    collections::{HashSet, VecDeque},
+    cmp::Reverse,
+    collections::{BinaryHeap, HashSet},
     io,
-    rc::Rc,
 };
+
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+
+#[derive(Debug, Clone, Copy, EnumIter)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn key(&self) -> char {
+        match self {
+            Direction::Up => '^',
+            Direction::Down => 'v',
+            Direction::Left => '<',
+            Direction::Right => '>',
+        }
+    }
+
+    fn vectored(&self, from: (usize, usize)) -> (usize, usize) {
+        match self {
+            Direction::Up => (from.0 - 1, from.1),
+            Direction::Down => (from.0 + 1, from.1),
+            Direction::Left => (from.0, from.1 - 1),
+            Direction::Right => (from.0, from.1 + 1),
+        }
+    }
+}
 
 trait Keypad<const COLS: usize, const ROWS: usize> {
     const LAYOUT: [[Option<char>; COLS]; ROWS];
 
-    fn get_parent(&mut self) -> Option<&mut DirectionalKeypad>;
+    fn get_parent(&self) -> Option<&DirectionalKeypad>;
 
-    fn neighbours(position: (usize, usize)) -> impl Iterator<Item = ((usize, usize), char)> {
-        [
-            (position.0 - 1, position.1),
-            (position.0 + 1, position.1),
-            (position.0, position.1 - 1),
-            (position.0, position.1 + 1),
-        ]
-        .into_iter()
-        .filter_map(
-            |neighbour| match Self::LAYOUT.get(neighbour.0)?.get(neighbour.1) {
-                Some(char_opt) => char_opt.and_then(|char| Some((neighbour, char))),
-                None => None,
-            },
-        )
+    fn get_parent_mut(&mut self) -> Option<&mut DirectionalKeypad>;
+
+    fn get_position(&self) -> (usize, usize);
+
+    fn set_position(&mut self, new_position: (usize, usize));
+
+    fn key_pos(key: Option<char>) -> Option<(usize, usize)> {
+        Self::LAYOUT.iter().enumerate().find_map(|(y, row)| {
+            row.iter()
+                .enumerate()
+                .find_map(|(x, row_key)| (*row_key == key).then_some((y, x)))
+        })
     }
 
-    fn shortest_path(from: (usize, usize), key: char) -> Vec<(usize, usize)> {
-        let mut queue = VecDeque::from([(from, Vec::new())]);
+    fn neighbours(
+        position: (usize, usize),
+    ) -> impl Iterator<Item = (Direction, (usize, usize), char)> {
+        Direction::iter()
+            .map(move |direction| (direction, direction.vectored(position)))
+            .filter_map(|(direction, neighbour)| {
+                match Self::LAYOUT.get(neighbour.0)?.get(neighbour.1) {
+                    Some(char_opt) => char_opt.and_then(|char| Some((direction, neighbour, char))),
+                    None => None,
+                }
+            })
+    }
+
+    /// Presses the given button, returning the cost of pressing that button.
+    fn press(&mut self, key: char) -> u64 {
+        let cost = self.shortest_path(self.get_position(), key).0;
+
+        self.set_position(Self::key_pos(Some(key)).unwrap());
+
+        cost
+    }
+
+    fn shortest_path(&self, from: (usize, usize), key: char) -> (u64, Vec<(usize, usize)>) {
+        let mut queue =
+            BinaryHeap::from([Reverse((0, from, Vec::new(), self.get_parent().cloned()))]);
         let mut visited = HashSet::new();
 
-        while let Some((position, path)) = queue.pop_front() {
+        while let Some(Reverse((cost, position, path, parent))) = queue.pop() {
             if !visited.insert(position) {
                 continue;
             }
@@ -39,19 +90,45 @@ trait Keypad<const COLS: usize, const ROWS: usize> {
             new_path.push(position);
 
             if Self::LAYOUT[position.0][position.1].unwrap() == key {
-                return new_path;
+                return (cost, new_path);
             }
 
-            let neighbours =
-                Self::neighbours(position).map(|neighbour| (neighbour.0, new_path.clone()));
+            let neighbours = Self::neighbours(position).map(|(direction, neighbour_pos, _)| {
+                Reverse((
+                    cost + parent
+                        .as_ref()
+                        .and_then(|parent| {
+                            Some(parent.shortest_path(parent.position, direction.key()).0)
+                        })
+                        .unwrap_or(1),
+                    neighbour_pos,
+                    new_path.clone(),
+                    self.get_parent().and_then(|parent| {
+                        Some(parent.with_position(
+                            DirectionalKeypad::key_pos(Some(direction.key())).unwrap(),
+                        ))
+                    }),
+                ))
+            });
 
             queue.extend(neighbours);
         }
 
         panic!("Unable to find path!")
     }
+
+    fn with_position(&self, position: (usize, usize)) -> Self
+    where
+        Self: Clone,
+    {
+        let mut temp = self.clone();
+        temp.set_position(position);
+
+        temp
+    }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 struct NumericKeypad {
     position: (usize, usize),
     parent: DirectionalKeypad,
@@ -66,8 +143,20 @@ impl Keypad<3, 4> for NumericKeypad {
         [None,      Some('0'), Some('A')],
     ];
 
-    fn get_parent(&mut self) -> Option<&mut DirectionalKeypad> {
+    fn get_parent(&self) -> Option<&DirectionalKeypad> {
+        Some(&self.parent)
+    }
+
+    fn get_parent_mut(&mut self) -> Option<&mut DirectionalKeypad> {
         Some(&mut self.parent)
+    }
+
+    fn get_position(&self) -> (usize, usize) {
+        self.position
+    }
+
+    fn set_position(&mut self, new_position: (usize, usize)) {
+        self.position = new_position;
     }
 }
 
@@ -80,6 +169,7 @@ impl NumericKeypad {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 struct DirectionalKeypad {
     position: (usize, usize),
     parent: Option<Box<DirectionalKeypad>>,
@@ -92,8 +182,20 @@ impl Keypad<3, 2> for DirectionalKeypad {
         [Some('<'), Some('v'), Some('>')]
     ];
 
-    fn get_parent(&mut self) -> Option<&mut DirectionalKeypad> {
+    fn get_parent(&self) -> Option<&DirectionalKeypad> {
+        self.parent.as_deref()
+    }
+
+    fn get_parent_mut(&mut self) -> Option<&mut DirectionalKeypad> {
         self.parent.as_deref_mut()
+    }
+
+    fn get_position(&self) -> (usize, usize) {
+        self.position
+    }
+
+    fn set_position(&mut self, new_position: (usize, usize)) {
+        self.position = new_position;
     }
 }
 
@@ -109,11 +211,21 @@ impl DirectionalKeypad {
 fn main() {
     let input = io::stdin().lines().map(Result::unwrap).collect::<Vec<_>>();
 
-    let part_one = input.iter().map(|code| {
-        let keypad_chain = NumericKeypad::new(DirectionalKeypad::new(Some(
-            DirectionalKeypad::new(Some(DirectionalKeypad::new(None))),
-        )));
+    let part_one: u64 = input
+        .iter()
+        .map(|code| {
+            let mut keypad_chain = NumericKeypad::new(DirectionalKeypad::new(Some(
+                DirectionalKeypad::new(Some(DirectionalKeypad::new(None))),
+            )));
 
-        todo!()
-    });
+            let numeric_part = code[..code.len() - 1].parse::<u64>().unwrap();
+
+            code.chars()
+                .map(|char| keypad_chain.press(char))
+                .sum::<u64>()
+                + numeric_part
+        })
+        .sum();
+
+    println!("{part_one}");
 }
