9cb26afe800a0ff12bb6bc479466c9bba39a4f38
James Harvey <44349936+jmshrv@users.noreply.github.com>
2025-12-03 17:50:53 +0000

    AoC 2025 Day 3 Part 2

--- advent-of-code/2025/day3/src/main.rs
+++ advent-of-code/2025/day3/src/main.rs
@@ -1,15 +1,30 @@
 use std::io;
 
-fn joltages(bank: &[u32]) -> impl Iterator<Item = u32> {
-    (0..bank.len() - 1).flat_map(move |i| {
-        (i..bank.len()).filter_map(move |j| {
-            if i == j {
-                None
-            } else {
-                Some(bank[i] * 10 + bank[j])
-            }
-        })
-    })
+fn max_joltage(bank: &[u64], battery_count: usize) -> u64 {
+    // Index that we last picked a battery from
+    let mut last_index = 0;
+
+    let mut batteries = vec![];
+
+    for remaining in (0..battery_count).rev() {
+        let (new_index, best_battery) = bank[last_index..]
+            .iter()
+            .enumerate()
+            .filter(|(index, _)| {
+                last_index + index + remaining < bank.len()
+            })
+            .rev() // Needed so that max always picks the first battery
+            .max_by_key(|(_, battery)| *battery)
+            .unwrap();
+
+        last_index += new_index + 1;
+        batteries.push(*best_battery);
+    }
+
+    batteries
+        .into_iter()
+        .reduce(|acc, e| acc * 10 + e)
+        .unwrap_or(0)
 }
 
 fn main() {
@@ -18,12 +33,16 @@ fn main() {
         .map(Result::unwrap)
         .map(|line| {
             line.chars()
-                .map(|c| c.to_digit(10).unwrap())
+                .map(|c| c.to_digit(10).unwrap() as u64)
                 .collect::<Vec<_>>()
         })
         .collect::<Vec<_>>();
 
-    let part1_answer: u32 = input.iter().map(|bank| joltages(bank).max().unwrap()).sum();
+    let part1_answer: u64 = input.iter().map(|bank| max_joltage(bank, 2)).sum();
 
     println!("{part1_answer}");
+
+    let part2_answer: u64 = input.iter().map(|bank| max_joltage(bank, 12)).sum();
+
+    println!("{part2_answer}");
 }
