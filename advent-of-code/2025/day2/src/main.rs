573387f8c09f14f6db130419ae62daa459891704
James Harvey <44349936+jmshrv@users.noreply.github.com>
2025-12-03 02:08:57 +0000

    AoC 2025 Day 2 Part 2

--- advent-of-code/2025/day2/src/main.rs
+++ advent-of-code/2025/day2/src/main.rs
@@ -3,6 +3,21 @@ use std::{
     ops::RangeInclusive,
 };
 
+use itertools::Itertools;
+
+fn digit_slices(id: usize, slice_count: usize) -> Vec<usize> {
+    // yeah I can't be bothered to do the maths here
+
+    let id_str = id.to_string();
+
+    id_str
+        .chars()
+        .chunks(slice_count)
+        .into_iter()
+        .map(|chunk| chunk.collect::<String>().parse().unwrap())
+        .collect()
+}
+
 fn is_mirror(id: usize) -> bool {
     let digits = id.ilog10() + 1;
 
@@ -22,6 +37,26 @@ fn mirror_ids(ids: RangeInclusive<usize>) -> impl Iterator<Item = usize> {
     ids.filter(|id| is_mirror(*id))
 }
 
+fn part2_invalid_ids(ids: RangeInclusive<usize>) -> impl Iterator<Item = usize> {
+    ids.filter(|id| {
+        let digits = id.ilog10() + 1;
+        (1..=digits).any(|chunk_count| {
+            if digits % chunk_count != 0 {
+                return false;
+            }
+
+            let slices = digit_slices(*id, chunk_count as usize);
+            let slice_counts = slices.iter().counts();
+
+            if slice_counts.len() != 1 {
+                return false;
+            }
+
+            *slice_counts.values().next().unwrap() >= 2
+        })
+    })
+}
+
 fn main() {
     let input = io::stdin()
         .lines()
@@ -38,4 +73,11 @@ fn main() {
     let part1_answer: usize = input.iter().flat_map(|ids| mirror_ids(ids.clone())).sum();
 
     println!("{part1_answer}");
+
+    let part2_answer: usize = input
+        .iter()
+        .flat_map(|ids| part2_invalid_ids(ids.clone()))
+        .sum();
+
+    println!("{part2_answer}");
 }
