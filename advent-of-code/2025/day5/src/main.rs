4213386997a4791c791968b20a6d6d7ed000bc13
James Harvey <44349936+jmshrv@users.noreply.github.com>
2025-12-09 00:19:37 +0000

    AoC 2025 Day 5 Part 2 (ugly but works)

--- advent-of-code/2025/day5/src/main.rs
+++ advent-of-code/2025/day5/src/main.rs
@@ -1,6 +1,48 @@
-use std::io;
+use std::{collections::HashSet, io, ops::RangeInclusive};
 
-use itertools::Itertools;
+fn merge_ranges(ranges: Vec<RangeInclusive<u64>>) -> HashSet<RangeInclusive<u64>> {
+    let mut merged_ranges = ranges.clone().into_iter().collect::<HashSet<_>>();
+
+    let mut overlaps_exist = true;
+
+    while overlaps_exist {
+        overlaps_exist = false;
+
+        for range in merged_ranges.clone() {
+            // We have the awkward and_then because we can't take a reference while messing with the
+            // HashSet later on
+            let maybe_intersect = merged_ranges
+                .iter()
+                .find(|merged_range| {
+                    if range.start() == merged_range.start() && range.end() == merged_range.end() {
+                        return false;
+                    }
+
+                    (range.start() >= merged_range.start() && range.start() <= merged_range.end())
+                        || (range.end() >= merged_range.start()
+                            && range.end() <= merged_range.end())
+                })
+                .and_then(|intersect| Some(intersect.clone()));
+
+            if let Some(intersect) = maybe_intersect {
+                overlaps_exist = true;
+
+                merged_ranges.remove(&range);
+                merged_ranges.remove(&intersect);
+
+                let new_start = range.start().min(intersect.start());
+                let new_end = range.end().max(intersect.end());
+
+                let new_range = *new_start..=*new_end;
+                merged_ranges.insert(new_range);
+            } else {
+                merged_ranges.insert(range.clone());
+            }
+        }
+    }
+
+    merged_ranges
+}
 
 fn main() {
     let input = io::read_to_string(io::stdin()).unwrap();
@@ -19,6 +61,16 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
+    let merged_ranges = merge_ranges(ranges);
+
+    // let calculated = merged_ranges
+    //     .iter()
+    //     .flat_map(|range| range.clone().into_iter())
+    //     .sorted()
+    //     .collect::<Vec<_>>();
+
+    // println!("{:?}", calculated);
+
     let ingredients = ingredients_str
         .lines()
         .map(str::parse::<u64>)
@@ -28,7 +80,7 @@ fn main() {
     let part1_answer = ingredients
         .iter()
         .filter(|ingredient| {
-            ranges
+            merged_ranges
                 .iter()
                 .find(|range| range.contains(ingredient))
                 .is_some()
@@ -37,11 +89,7 @@ fn main() {
 
     println!("{part1_answer}");
 
-    let part2_answer = ranges
-        .iter()
-        .flat_map(|range| range.clone().into_iter())
-        .unique()
-        .count();
+    let part2_answer: usize = merged_ranges.into_iter().map(|range| range.count()).sum();
 
     println!("{part2_answer}");
 }
