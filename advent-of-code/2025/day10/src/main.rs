1ebb63772f54619d72beb81d928cf3264021ca12
James Harvey <44349936+jmshrv@users.noreply.github.com>
2025-12-28 20:03:42 +0000

    AoC 2025 Day 10 Part 1

---
+++ advent-of-code/2025/day10/src/main.rs
@@ -0,0 +1,105 @@
+use std::{
+    collections::{HashSet, VecDeque},
+    io,
+};
+
+use lazy_regex::regex;
+
+struct Machine {
+    indicator_lights: Vec<bool>,
+    switches: Vec<Vec<usize>>,
+    joltages: Vec<usize>,
+}
+
+struct SearchState {
+    depth: u64,
+    indicator_lights: Vec<bool>,
+}
+
+fn parse_line(line: &str) -> Machine {
+    let regex = regex!(r"(\[(?:\.|#)*\])(?: )(\(.*\))(?: )(\{.*\})");
+
+    let (_, [indicator_lights_str, switches_str, joltages_str]) =
+        regex.captures(line).unwrap().extract::<3>();
+
+    let indicator_lights = indicator_lights_str
+        .chars()
+        .filter_map(|c| match c {
+            '.' => Some(false),
+            '#' => Some(true),
+            '[' | ']' => None,
+            _ => panic!("Invalid indicator light {c}"), // want to actually notice invalid
+        })
+        .collect();
+
+    let switches = switches_str
+        .split_ascii_whitespace()
+        .map(|switch| {
+            switch
+                .trim_start_matches('(')
+                .trim_end_matches(')')
+                .split(',')
+                .map(|idx| idx.parse().unwrap())
+                .collect()
+        })
+        .collect();
+
+    let joltages = joltages_str
+        .trim_start_matches('{')
+        .trim_end_matches('}')
+        .split(',')
+        .map(|joltage| joltage.parse().unwrap())
+        .collect();
+
+    Machine {
+        indicator_lights,
+        switches,
+        joltages,
+    }
+}
+
+fn button_presses(machine: &Machine) -> u64 {
+    let mut queue = VecDeque::from([SearchState {
+        depth: 0,
+        indicator_lights: vec![false; machine.indicator_lights.len()],
+    }]);
+
+    let mut seen = HashSet::new();
+
+    while let Some(state) = queue.pop_front() {
+        if state.indicator_lights == machine.indicator_lights {
+            return state.depth;
+        }
+
+        if !seen.insert(state.indicator_lights.clone()) {
+            continue;
+        }
+
+        for switch in &machine.switches {
+            let mut new_lights = state.indicator_lights.clone();
+
+            for &idx in switch {
+                new_lights[idx] = !new_lights[idx];
+            }
+
+            queue.push_back(SearchState {
+                depth: state.depth + 1,
+                indicator_lights: new_lights,
+            });
+        }
+    }
+
+    unreachable!()
+}
+
+fn main() {
+    let input = io::stdin()
+        .lines()
+        .map(Result::unwrap)
+        .map(|line| parse_line(&line))
+        .collect::<Vec<_>>();
+
+    let part1_answer: u64 = input.iter().map(|machine| button_presses(machine)).sum();
+
+    println!("{part1_answer}");
+}
