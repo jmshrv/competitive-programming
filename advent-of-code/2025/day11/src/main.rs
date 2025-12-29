6d2f9a4f26e2e5bc654390dfc3a334f88877bcff
James Harvey <44349936+jmshrv@users.noreply.github.com>
2025-12-29 22:17:30 +0000

    AoC 2025 Day 11 Part 2 (in theory)

--- advent-of-code/2025/day11/src/main.rs
+++ advent-of-code/2025/day11/src/main.rs
@@ -1,10 +1,11 @@
 use std::{
-    collections::{HashMap, VecDeque},
+    collections::{HashMap, HashSet, VecDeque},
     io,
 };
 
-fn paths(routes: &HashMap<String, Vec<String>>) -> u64 {
+fn part1_paths(routes: &HashMap<String, Vec<String>>) -> u64 {
     let mut queue = VecDeque::from(routes["you"].clone());
+
     let mut path_count = 0;
 
     // Just a bfs without a seen set lol
@@ -18,6 +19,37 @@ fn paths(routes: &HashMap<String, Vec<String>>) -> u64 {
 
         for next_path in &routes[&path] {
             queue.push_back(next_path.clone());
+        }
+    }
+
+    path_count
+}
+
+fn part2_paths(routes: &HashMap<String, Vec<String>>) -> u64 {
+    let mut queue = routes["svr"]
+        .iter()
+        .map(|server| (server.clone(), false, false))
+        .collect::<VecDeque<_>>();
+
+    let mut path_count = 0;
+
+    while let Some((path, seen_dac, seen_fft)) = queue.pop_front() {
+        if seen_dac && seen_fft && path == "out" {
+            path_count += 1;
+        }
+
+        // This is in a separate check since we still need to continue on "out" when we haven't seen
+        // a DAC and an FFT
+        if path == "out" {
+            continue;
+        }
+
+        for next_path in &routes[&path] {
+            queue.push_back((
+                next_path.clone(),
+                seen_dac || path == "dac",
+                seen_fft || path == "fft",
+            ));
         }
     }
 
@@ -40,6 +72,9 @@ fn main() {
         })
         .collect::<HashMap<_, _>>();
 
-    let part1_answer = paths(&input);
+    let part1_answer = part1_paths(&input);
     println!("{part1_answer}");
+
+    let part2_answer = part2_paths(&input);
+    println!("{part2_answer}");
 }
