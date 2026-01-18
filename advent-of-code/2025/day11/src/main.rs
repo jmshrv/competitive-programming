d6d7323a28168b99318ed1d0b3ecbdee150e8805
James Harvey <44349936+jmshrv@users.noreply.github.com>
2026-01-18 21:38:33 +0000

    Use cached instead of manually doing cache
    Not actually using this since part 1 and part 2 should probably have fresh caches

--- advent-of-code/2025/day11/src/main.rs
+++ advent-of-code/2025/day11/src/main.rs
@@ -1,17 +1,12 @@
 use std::{collections::HashMap, io};
 
-// why of course we need to optimally use string refs everywhere
-fn paths<'a>(
-    routes: &'a HashMap<String, Vec<String>>,
-    cache: &mut HashMap<(&'a str, bool, bool), u64>,
-    node: &'a str,
-    seen_dac: bool,
-    seen_fft: bool,
-) -> u64 {
-    if let Some(&cached) = cache.get(&(node, seen_dac, seen_fft)) {
-        return cached;
-    }
+use cached::proc_macro::cached;
 
+#[cached(
+    key = "(String, bool, bool)",
+    convert = r#"{ (node.to_string(), seen_dac, seen_fft) }"#
+)]
+fn paths(routes: &HashMap<String, Vec<String>>, node: &str, seen_dac: bool, seen_fft: bool) -> u64 {
     let mut valid_routes = 0;
 
     if node == "out" {
@@ -21,14 +16,12 @@ fn paths<'a>(
     for child in &routes[node] {
         let res = paths(
             routes,
-            cache,
             child,
             seen_dac || node == "dac",
             seen_fft || node == "fft",
         );
 
         valid_routes += res;
-        cache.insert((child, seen_dac, seen_fft), res);
     }
 
     valid_routes
@@ -50,9 +43,9 @@ fn main() {
         })
         .collect::<HashMap<_, _>>();
 
-    let part1_answer = paths(&input, &mut HashMap::new(), "you", true, true);
+    let part1_answer = paths(&input, "you", true, true);
     println!("{part1_answer}");
 
-    let part2_answer = paths(&input, &mut HashMap::new(), "svr", false, false);
+    let part2_answer = paths(&input, "svr", false, false);
     println!("{part2_answer}");
 }
